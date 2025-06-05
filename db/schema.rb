f2adc4a855f6b51add60e80f9288b0683d3e7d95
Jibril Tapiador <tapiador@jib.is>
2025-06-06 04:15:53 +0800

    feat: create cart_items table with migration and update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_05_200551) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_05_201315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id", "product_id"], name: "index_cart_items_on_cart_id_and_product_id", unique: true
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,4 +38,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_05_200551) do
     t.datetime "updated_at", null: false
     t.index ["sku"], name: "index_products_on_sku", unique: true
   end
+
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
