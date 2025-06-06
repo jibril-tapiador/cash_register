000aa9a71a0ba301d7118d7e2fc7bc4497d25e64
Jibril Tapiador <tapiador@jib.is>
2025-06-06 10:15:39 +0800

    chore: add default product seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.create!([
+  { name: 'Green Tea',    sku: 'GR1', price: 3.11 },
+  { name: 'Strawberries', sku: 'SR1', price: 5.00 },
+  { name: 'Coffee',       sku: 'CF1', price: 11.23 }
+])
